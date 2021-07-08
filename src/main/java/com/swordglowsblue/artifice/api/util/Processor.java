695ed0a930c46eb7951e28a5faa2d8b291a4ade9
CmdrNorthpaw <thetuxkid@gmail.com>
2021-07-08 20:17:02 +0100

    Deprecate Processor in favour of a Kotlin lambda

--- src/main/java/com/swordglowsblue/artifice/api/util/Processor.java
+++ src/main/java/com/swordglowsblue/artifice/api/util/Processor.java
@@ -1,9 +1,12 @@
 package com.swordglowsblue.artifice.api.util;
 
+import kotlin.Deprecated;
+
 import java.util.function.Consumer;
 
 /** A wrapper around {@link Consumer} that returns its argument instead of {@code void}.
  *  @param <T> The type to be passed in and returned. */
+@Deprecated(message = "Please use a Kotlin lambda instead")
 @FunctionalInterface
 public interface Processor<T> extends Consumer<T> {
     default T process(T t) {
