e0195f2f449b227f75e9717bf627dc8de7ee3810
CmdrNorthpaw <thetuxkid@gmail.com>
2021-07-12 19:55:59 +0100

    Update StateDataBuilder to Kotlin

--- src/main/java/com/swordglowsblue/artifice/api/builder/data/StateDataBuilder.kt
+++ src/main/java/com/swordglowsblue/artifice/api/builder/data/StateDataBuilder.kt
@@ -1,24 +1,21 @@
-package com.swordglowsblue.artifice.api.builder.data;
+package com.swordglowsblue.artifice.api.builder.data
 
-import com.google.gson.JsonObject;
-import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder;
+import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder
+import com.google.gson.JsonObject
+import com.swordglowsblue.artifice.api.builder.data.StateDataBuilder
+import java.util.function.Function
 
-public class StateDataBuilder extends TypedJsonBuilder<JsonObject> {
-
-    private JsonObject jsonObject = new JsonObject();
-
-    public StateDataBuilder() {
-        super(new JsonObject(), j->j);
-    }
+class StateDataBuilder : TypedJsonBuilder<JsonObject?>(JsonObject(), Function { j: JsonObject? -> j }) {
+    private val jsonObject = JsonObject()
 
     /**
      * Set the id of the block.
      * @param id
      * @return
      */
-    public StateDataBuilder name(String id) {
-        this.root.addProperty("Name", id);
-        return this;
+    fun name(id: String?): StateDataBuilder {
+        this.root.addProperty("Name", id)
+        return this
     }
 
     /**
@@ -27,9 +24,9 @@ public class StateDataBuilder extends TypedJsonBuilder<JsonObject> {
      * @param state
      * @return
      */
-    public StateDataBuilder setProperty(String property, String state) {
-        this.jsonObject.addProperty(property, state);
-        this.root.add("Properties", this.jsonObject);
-        return this;
+    fun setProperty(property: String?, state: String?): StateDataBuilder {
+        jsonObject.addProperty(property, state)
+        this.root.add("Properties", jsonObject)
+        return this
     }
 }
