fbc1335d3a4e635f135b95862564802c5020d0eb
CmdrNorthpaw <thetuxkid@gmail.com>
2021-07-15 13:28:23 +0100

    Make a start on Configured builders

--- src/main/java/com/swordglowsblue/artifice/api/builder/data/worldgen/configured/ConfiguredCarverBuilder.kt
+++ src/main/java/com/swordglowsblue/artifice/api/builder/data/worldgen/configured/ConfiguredCarverBuilder.kt
@@ -1,33 +1,34 @@
-package com.swordglowsblue.artifice.api.builder.data.worldgen.configured;
+package com.swordglowsblue.artifice.api.builder.data.worldgen.configured
 
-import com.google.gson.JsonObject;
-import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder;
-import com.swordglowsblue.artifice.api.resource.JsonResource;
+import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder
+import com.swordglowsblue.artifice.api.resource.JsonResource
+import com.google.gson.JsonObject
+import com.swordglowsblue.artifice.api.builder.data.worldgen.configured.ConfiguredCarverBuilder
+import com.swordglowsblue.artifice.api.builder.data.StateDataBuilder
+import java.util.function.Function
 
-public class ConfiguredCarverBuilder extends TypedJsonBuilder<JsonResource<JsonObject>> {
-    public ConfiguredCarverBuilder() {
-        super(new JsonObject(), JsonResource::new);
-    }
-
+class ConfiguredCarverBuilder : TypedJsonBuilder<JsonResource<JsonObject?>?>(
+    JsonObject(),
+    Function<JsonObject?, JsonResource<JsonObject?>?> { root: JsonObject? -> JsonResource(root) }) {
     /**
      * @param id ID of an existing carver.
      * @return this
      */
-    public ConfiguredCarverBuilder name(String id) {
-        this.root.addProperty("type", id);
-        return this;
+    fun name(id: String?): ConfiguredCarverBuilder {
+        this.root.addProperty("type", id)
+        return this
     }
 
-    public ConfiguredCarverBuilder probability(float probability) {
+    fun probability(probability: Float): ConfiguredCarverBuilder {
         try {
-            if (probability > 1.0F) throw new Throwable("Probability can't be higher than 1.0F! Found " + probability);
-            if (probability < 0.0F) throw new Throwable("Probability can't be smaller than 0.0F! Found " + probability);
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("probability", probability);
-            this.root.add("config", jsonObject);
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
+            if (probability > 1.0f) throw Throwable("Probability can't be higher than 1.0F! Found $probability")
+            if (probability < 0.0f) throw Throwable("Probability can't be smaller than 0.0F! Found $probability")
+            val jsonObject = JsonObject()
+            jsonObject.addProperty("probability", probability)
+            this.root.add("config", jsonObject)
+        } catch (throwable: Throwable) {
+            throwable.printStackTrace()
         }
-        return this;
+        return this
     }
 }
--- src/main/java/com/swordglowsblue/artifice/api/builder/data/worldgen/configured/ConfiguredSurfaceBuilder.kt
+++ src/main/java/com/swordglowsblue/artifice/api/builder/data/worldgen/configured/ConfiguredSurfaceBuilder.kt
@@ -1,43 +1,52 @@
-package com.swordglowsblue.artifice.api.builder.data.worldgen.configured;
+package com.swordglowsblue.artifice.api.builder.data.worldgen.configured
 
-import com.google.gson.JsonObject;
-import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder;
-import com.swordglowsblue.artifice.api.builder.data.StateDataBuilder;
-import com.swordglowsblue.artifice.api.resource.JsonResource;
-import com.swordglowsblue.artifice.api.util.Processor;
+import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder
+import com.swordglowsblue.artifice.api.resource.JsonResource
+import com.google.gson.JsonObject
+import com.swordglowsblue.artifice.api.builder.data.worldgen.configured.ConfiguredCarverBuilder
+import com.swordglowsblue.artifice.api.builder.data.StateDataBuilder
+import java.util.function.Function
 
-public class ConfiguredSurfaceBuilder extends TypedJsonBuilder<JsonResource<JsonObject>> {
-    public ConfiguredSurfaceBuilder() {
-        super(new JsonObject(), JsonResource::new);
-        this.root.add("config", new JsonObject());
-    }
-
+class ConfiguredSurfaceBuilder : TypedJsonBuilder<JsonResource<JsonObject?>?>(
+    JsonObject(),
+    Function<JsonObject?, JsonResource<JsonObject?>?> { root: JsonObject? -> JsonResource(root) }) {
     /**
      * Set a block state.
      * @param id
      * @param blockStateBuilderProcessor
      * @return
      */
-    private ConfiguredSurfaceBuilder setBlockState(String id, Processor<StateDataBuilder> blockStateBuilderProcessor) {
-        with(this.root.getAsJsonObject("config"),id, JsonObject::new, jsonObject -> blockStateBuilderProcessor.process(new StateDataBuilder()).buildTo(jsonObject));
-        return this;
+    private fun setBlockState(
+        id: String,
+        blockStateBuilderProcessor: StateDataBuilder.() -> Unit
+    ): ConfiguredSurfaceBuilder {
+        with(
+            this.root.getAsJsonObject("config"),
+            id,
+            { JsonObject() }) { jsonObject: JsonObject? ->
+            StateDataBuilder().apply(blockStateBuilderProcessor).buildTo(jsonObject)
+        }
+        return this
     }
 
-    public ConfiguredSurfaceBuilder topMaterial(Processor<StateDataBuilder> blockStateBuilderProcessor) {
-        return this.setBlockState("top_material", blockStateBuilderProcessor);
+    fun topMaterial(blockStateBuilderProcessor: StateDataBuilder.() -> Unit): ConfiguredSurfaceBuilder {
+        return setBlockState("top_material", blockStateBuilderProcessor)
     }
 
-    public ConfiguredSurfaceBuilder underMaterial(Processor<StateDataBuilder> blockStateBuilderProcessor) {
-        return this.setBlockState("under_material", blockStateBuilderProcessor);
+    fun underMaterial(blockStateBuilderProcessor: StateDataBuilder.() -> Unit): ConfiguredSurfaceBuilder {
+        return setBlockState("under_material", blockStateBuilderProcessor)
     }
 
-    public ConfiguredSurfaceBuilder underwaterMaterial(Processor<StateDataBuilder> blockStateBuilderProcessor) {
-        return this.setBlockState("underwater_material", blockStateBuilderProcessor);
+    fun underwaterMaterial(blockStateBuilderProcessor: StateDataBuilder.() -> Unit): ConfiguredSurfaceBuilder {
+        return setBlockState("underwater_material", blockStateBuilderProcessor)
     }
 
-    public ConfiguredSurfaceBuilder surfaceBuilderID(String id) {
-        this.root.addProperty("type", id);
-        return this;
+    fun surfaceBuilderID(id: String?): ConfiguredSurfaceBuilder {
+        this.root.addProperty("type", id)
+        return this
     }
 
+    init {
+        this.root.add("config", JsonObject())
+    }
 }
