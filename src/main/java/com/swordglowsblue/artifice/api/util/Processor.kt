ecb4d66af0f3296aae182eadeac97b434a3a8c46
CmdrNorthpaw <thetuxkid@gmail.com>
2021-07-15 21:48:05 +0100

    Add a ReplaceWith to Processor's deprecation

--- src/main/java/com/swordglowsblue/artifice/api/util/Processor.kt
+++ src/main/java/com/swordglowsblue/artifice/api/util/Processor.kt
@@ -5,7 +5,7 @@ import java.util.function.Consumer
 /** A wrapper around [Consumer] that returns its argument instead of `void`.
  * @param <T> The type to be passed in and returned.
 </T> */
-@Deprecated(message = "Please use a Kotlin lambda instead")
+@Deprecated(message = "Please use a Kotlin lambda instead", replaceWith = ReplaceWith("Builder<T>"))
 @FunctionalInterface
 fun interface Processor<T> : Consumer<T> {
     fun process(t: T): T {
