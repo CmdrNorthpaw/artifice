46ea15a20a15a15f3e6c857901e281257b5bc998
CmdrNorthpaw <thetuxkid@gmail.com>
2021-07-16 08:59:41 +0100

    Update IdUtils to Kotlin

--- src/main/java/com/swordglowsblue/artifice/api/util/IdUtils.kt
+++ src/main/java/com/swordglowsblue/artifice/api/util/IdUtils.kt
@@ -1,45 +1,42 @@
-package com.swordglowsblue.artifice.api.util;
+package com.swordglowsblue.artifice.api.util
 
-import net.minecraft.util.Identifier;
+import net.minecraft.util.Identifier
 
-/** Utilities for modifying {@link Identifier}s. */
-public final class IdUtils {
+/** Utilities for modifying [Identifier]s.  */
+object IdUtils {
     /**
-     * Add a prefix to the path of the given {@link Identifier}.
-     * @param prefix The prefix to add.
-     * @param id The base ID.
-     * @return A new {@link Identifier} with the prefixed path.
-     */
-    public static Identifier wrapPath(String prefix, Identifier id) { return wrapPath(prefix, id, ""); }
-
-    /**
-     * Add a suffix to the path of the given {@link Identifier}.
+     * Add a suffix to the path of the given [Identifier].
      * @param id The base ID.
      * @param suffix The suffix to add.
-     * @return A new {@link Identifier} with the suffixed path.
+     * @return A new [Identifier] with the suffixed path.
      */
-    public static Identifier wrapPath(Identifier id, String suffix) { return wrapPath("", id, suffix); }
-
+    fun wrapPath(id: Identifier, suffix: String): Identifier {
+        return wrapPath("", id, suffix)
+    }
     /**
-     * Add a prefix and suffix to the path of the given {@link Identifier}.
+     * Add a prefix and suffix to the path of the given [Identifier].
      * @param prefix The prefix to add.
      * @param id The base ID.
      * @param suffix The suffix to add.
-     * @return A new {@link Identifier} with the wrapped path.
+     * @return A new [Identifier] with the wrapped path.
      */
-    public static Identifier wrapPath(String prefix, Identifier id, String suffix) {
-        if(prefix.isEmpty() && suffix.isEmpty()) return id;
-        return new Identifier(id.getNamespace(), prefix+id.getPath()+suffix);
+    @JvmStatic
+    @JvmOverloads
+    fun wrapPath(prefix: String, id: Identifier, suffix: String = ""): Identifier {
+        return if (prefix.isEmpty() && suffix.isEmpty()) id else Identifier(
+            id.namespace,
+            prefix + id.path + suffix
+        )
     }
 
     /**
-     * If the given {@link Identifier} has the namespace "minecraft" (the default namespace),
-     *   return a copy with the given {@code defaultNamespace}. Otherwise, return the ID unchanged.
+     * If the given [Identifier] has the namespace "minecraft" (the default namespace),
+     * return a copy with the given `defaultNamespace`. Otherwise, return the ID unchanged.
      * @param id The base ID.
-     * @param defaultNamespace The namespace to replace {@code minecraft} with if applicable.
+     * @param defaultNamespace The namespace to replace `minecraft` with if applicable.
      * @return The given ID with its namespace replaced if applicable.
      */
-    public static Identifier withDefaultNamespace(Identifier id, String defaultNamespace) {
-        return id.getNamespace().equals("minecraft") ? new Identifier(defaultNamespace, id.getPath()) : id;
+    fun withDefaultNamespace(id: Identifier, defaultNamespace: String): Identifier {
+        return if (id.namespace == "minecraft") Identifier(defaultNamespace, id.path) else id
     }
 }
