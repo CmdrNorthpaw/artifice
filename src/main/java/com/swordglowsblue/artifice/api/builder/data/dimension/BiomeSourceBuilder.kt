3cf431db77e51d603e7a1772f8eaa0c6d1ef56b4
CmdrNorthpaw <thetuxkid@gmail.com>
2021-08-02 14:22:19 +0100

    Use DSL in BiomeSourceBuilder

--- src/main/java/com/swordglowsblue/artifice/api/builder/data/dimension/BiomeSourceBuilder.kt
+++ src/main/java/com/swordglowsblue/artifice/api/builder/data/dimension/BiomeSourceBuilder.kt
@@ -3,8 +3,12 @@ package com.swordglowsblue.artifice.api.builder.data.dimension
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder
+import com.swordglowsblue.artifice.api.dsl.ArtificeDsl
+import com.swordglowsblue.artifice.api.util.Builder
+import com.swordglowsblue.artifice.api.util.IdUtils.asId
 import net.minecraft.util.Identifier
 import java.util.function.Function
+import kotlin.jvm.Throws
 
 sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
     type: Identifier
@@ -26,17 +30,11 @@ sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
         return me
     }
 
-    /**
-     * Set the type of the biome source.
-     * @param type
-     * @param <T>
-     * @return
-    </T> */
-    fun type(type: String): T {
-        this.root.addProperty("type", type)
-        return me
-    }
+    var seed: Int?
+    get() = root["seed"]?.asInt
+    set(value) = root.addProperty("seed", value)
 
+    @ArtificeDsl
     class VanillaLayeredBiomeSourceBuilder : BiomeSourceBuilder<VanillaLayeredBiomeSourceBuilder>(
         Identifier("vanilla_layered")
     ) {
@@ -47,21 +45,24 @@ sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
          * @param largeBiomes
          * @return
          */
-        fun largeBiomes(largeBiomes: Boolean): VanillaLayeredBiomeSourceBuilder {
-            this.root.addProperty("large_biomes", largeBiomes)
-            return this
-        }
+        fun largeBiomes(largeBiomes: Boolean) = apply { this.largeBiomes = largeBiomes }
+
+        var largeBiomes: Boolean?
+        get() = root["large_biomes"]?.asBoolean
+        set(value) = root.addProperty("large_biomes", value)
 
         /**
          * @param legacyBiomeInitLayer
          * @return
          */
-        fun legacyBiomeInitLayer(legacyBiomeInitLayer: Boolean): VanillaLayeredBiomeSourceBuilder {
-            this.root.addProperty("legacy_biome_init_layer", legacyBiomeInitLayer)
-            return this
-        }
+        fun legacyBiomeInitLayer(legacyBiomeInitLayer: Boolean) = apply { this.legacyBiomeInitLayer = legacyBiomeInitLayer }
+
+        var legacyBiomeInitLayer: Boolean?
+        get() = root["legacy_biome_init_layer"].asBoolean
+        set(value) = root.addProperty("legacy_biome_init_layer", value)
     }
 
+    @ArtificeDsl
     class MultiNoiseBiomeSourceBuilder : BiomeSourceBuilder<MultiNoiseBiomeSourceBuilder>(
         Identifier("multi_noise")
     ) {
@@ -78,108 +79,112 @@ sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
             return this
         }
 
+        var preset: String?
+        get() = root["preset"]?.asString
+        set(value) = root.addProperty("preset", value)
+
         /**
          * Add biome.
          * @param biomeBuilder
          * @return
          */
-        fun addBiome(biomeBuilder: BiomeBuilder.() -> Unit): MultiNoiseBiomeSourceBuilder {
-            with(
-                "biomes",
-                { JsonArray() }) { biomeArray: JsonArray ->
+        fun addBiome(biomeBuilder: Builder<BiomeBuilder>) = apply {
+            with("biomes", { JsonArray() }) { biomeArray: JsonArray ->
                 biomeArray.add(
                     BiomeBuilder().apply(biomeBuilder).buildTo(JsonObject())
                 )
             }
-            return this
         }
 
+        @ArtificeDsl
         class BiomeBuilder : TypedJsonBuilder<JsonObject>(JsonObject(), { j: JsonObject -> j }) {
             /**
              * Set the biome ID.
              * @param id
              * @return
              */
-            fun biome(id: String): BiomeBuilder {
-                this.root.addProperty("biome", id)
-                return this
-            }
+            fun id(id: Identifier) = apply { this.id = id }
+
+            var id: Identifier?
+            get() = root["biome"].asId
+            set(value) = root.addProperty("biome", value?.toString())
 
             /**
              * Build biome parameters.
              * @param biomeSettingsBuilder
              * @return
              */
-            fun parameters(biomeSettingsBuilder: BiomeParametersBuilder.() -> Unit): BiomeBuilder {
+            fun parameters(biomeSettingsBuilder: Builder<BiomeParametersBuilder>) = apply {
                 with("parameters", { JsonObject() }) { biomeSettings: JsonObject ->
                     BiomeParametersBuilder().apply(biomeSettingsBuilder).buildTo(biomeSettings)
                 }
-                return this
             }
         }
 
+        @ArtificeDsl
         class BiomeParametersBuilder : TypedJsonBuilder<JsonObject>(JsonObject(), { j: JsonObject -> j }) {
+            @Throws(IllegalArgumentException::class)
+            fun require2F(float: Float?, fieldName: String) = float?.apply {
+                require(float <= 2.0f) { "altitude can't be higher than     2! Found $float" }
+                require(float >= -2.0f) { "$fieldName can't be less than -2! Found $float" }
+            }
+
             /**
              * @param altitude
              * @return
              */
-            fun altitude(altitude: Float): BiomeParametersBuilder {
-                require(altitude <= 2.0f) { "altitude can't be higher than 2.0F! Found $altitude" }
-                require(altitude >= -2.0f) { "altitude can't be smaller than 2.0F! Found $altitude" }
-                this.root.addProperty("altitude", altitude)
-                return this
-            }
+            fun altitude(altitude: Float) = apply { this.altitude = altitude }
+
+            var altitude: Float?
+            get() = root["altitude"]?.asFloat
+            set(value) = root.addProperty("altitude", require2F(value, "altitude"))
 
             /**
              * @param weirdness
              * @return
              */
-            fun weirdness(weirdness: Float): BiomeParametersBuilder {
-                require(weirdness <= 2.0f) { "weirdness can't be higher than 2.0F! Found $weirdness" }
-                require(weirdness >= -2.0f) { "weirdness can't be smaller than 2.0F! Found $weirdness" }
-                this.root.addProperty("weirdness", weirdness)
-                return this
-            }
+            fun weirdness(weirdness: Float) = apply { this.weirdness = weirdness }
+
+            var weirdness: Float?
+            get() = root["weirdness"]?.asFloat
+            set(value) = root.addProperty("weirdness", require2F(value, "weirdness"))
 
             /**
              * @param offset
              * @return
              */
-            fun offset(offset: Float): BiomeParametersBuilder {
-                require(offset <= 1.0f) { "offset can't be higher than 1.0F! Found $offset" }
-                require(offset >= 0.0f) { "offset can't be smaller than 0.0F! Found $offset" }
-                this.root.addProperty("offset", offset)
-                return this
-            }
+            fun offset(offset: Float) = apply { this.offset = offset }
+
+            var offset: Float?
+            get() = root["offset"]?.asFloat
+            set(value) = root.addProperty("offset", require2F(value, "offset"))
 
             /**
              * @param temperature
              * @return
              */
-            fun temperature(temperature: Float): BiomeParametersBuilder {
-                require(temperature <= 2.0f) { "temperature can't be higher than 2.0F! Found $temperature" }
-                require(temperature >= -2.0f) { "temperature can't be smaller than 2.0F! Found $temperature" }
-                this.root.addProperty("temperature", temperature)
-                return this
-            }
+            fun temperature(temperature: Float) = apply { this.temperature = temperature }
+
+            var temperature: Float?
+            get() = root["temperature"]?.asFloat
+            set(value) = root.addProperty("temperature", require2F(value, "temperature"))
 
             /**
              * @param humidity
              * @return
              */
-            fun humidity(humidity: Float): BiomeParametersBuilder {
-                require(humidity <= 2.0f) { "humidity can't be higher than 2.0F! Found $humidity" }
-                require(humidity >= -2.0f) { "humidity can't be smaller than 2.0F! Found $humidity" }
-                this.root.addProperty("humidity", humidity)
-                return this
-            }
+            fun humidity(humidity: Float) = apply { this.humidity = humidity }
+
+            var humidity: Float?
+            get() = root["humidity"]?.asFloat
+            set(value) = root.addProperty("humidity", require2F(value, "humidity"))
         }
 
         /**
          * @param noiseSettings
          * @return this
          */
-        fun altitudeNoise(noiseSettings: NoiseSettings.() -> Unit): MultiNoiseBiomeSourceBuilder {
+        fun altitudeNoise(noiseSettings: NoiseSettings.() -> Unit) = apply {
             with("altitude_noise", { JsonObject() }) { jsonObject: JsonObject ->
                 NoiseSettings().apply(noiseSettings).buildTo(jsonObject)
             }
@@ -190,55 +195,53 @@ sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
          * @param noiseSettings
          * @return this
          */
-        fun weirdnessNoise(noiseSettings: NoiseSettings.() -> Unit): MultiNoiseBiomeSourceBuilder {
+        fun weirdnessNoise(noiseSettings: NoiseSettings.() -> Unit) = apply {
             with("weirdness_noise", { JsonObject() }) { jsonObject: JsonObject ->
                 NoiseSettings().apply(noiseSettings).buildTo(jsonObject)
             }
-            return this
         }
 
         /**
          * @param noiseSettings
          * @return this
          */
-        fun temperatureNoise(noiseSettings: NoiseSettings.() -> Unit): MultiNoiseBiomeSourceBuilder {
+        fun temperatureNoise(noiseSettings: NoiseSettings.() -> Unit) = apply {
             with("temperature_noise", { JsonObject() }) { jsonObject: JsonObject ->
                 NoiseSettings().apply(noiseSettings).buildTo(jsonObject)
             }
-            return this
         }
 
         /**
          * @param noiseSettings
          * @return this
          */
-        fun humidityNoise(noiseSettings: NoiseSettings.() -> Unit): MultiNoiseBiomeSourceBuilder {
+        fun humidityNoise(noiseSettings: NoiseSettings.() -> Unit) = apply {
             with("humidity_noise", { JsonObject() }) { jsonObject: JsonObject ->
                 NoiseSettings().apply(noiseSettings).buildTo(jsonObject)
             }
-            return this
         }
 
+        @ArtificeDsl
         class NoiseSettings : TypedJsonBuilder<JsonObject>(JsonObject(), { j: JsonObject -> j }) {
             /**
              * Changes how much detail the noise of the respective value has
              * @param octave how much detail the noise of the respective value has
              * @return this
              */
-            fun firstOctave(octave: Int): NoiseSettings {
-                this.root.addProperty("firstOctave", octave)
-                return this
-            }
+            fun firstOctave(octave: Int) = apply { firstOctave = octave }
+
+            var firstOctave: Int?
+            get() = root["firstOctave"]?.asInt
+            set(value) = root.addProperty("firstOctave", value)
 
             /**
              * @param amplitudes the amplitudes you want
              * @return this
              */
-            fun amplitudes(vararg amplitudes: Float): NoiseSettings {
+            fun amplitudes(vararg amplitudes: Float) = apply {
                 jsonArray("amplitudes") {
                     amplitudes.forEach { add(it) }
                 }
-                return this
             }
         }
 
@@ -248,14 +251,17 @@ sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
              * @param amplitude idk
              * @return
              */
-            fun amplitude(amplitude: Float): AmplitudesBuilder {
-                this.root.addProperty("altitude", amplitude)
-                return this
-            }
+            fun amplitude(amplitude: Float) = apply { this.amplitude = amplitude }
+
+            var amplitude: Float?
+            get() = root["amplitude"]?.asFloat
+            set(value) = root.addProperty("amplitude", value)
         }
     }
 
-    class CheckerboardBiomeSourceBuilder : BiomeSourceBuilder<CheckerboardBiomeSourceBuilder>(Identifier("checkerboard")) {
+    @ArtificeDsl
+    class CheckerboardBiomeSourceBuilder :
+        BiomeSourceBuilder<CheckerboardBiomeSourceBuilder>(Identifier("checkerboard")) {
         override val me: CheckerboardBiomeSourceBuilder
             get() = this
 
@@ -264,10 +270,16 @@ sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
          * @return
          */
         fun scale(scale: Int): CheckerboardBiomeSourceBuilder {
-            require(scale <= 62) { "Scale can't be higher than 62! Found $scale" }
-            require(scale >= 0) { "Scale can't be smaller than 0! Found $scale" }
-            this.root.addProperty("scale", scale)
             return this
+        }
+
+        var scale: Int?
+        get() = root["scale"].asInt
+        set(value) {
+            requireNotNull(value)
+            require(value <= 62) { "Scale can't be higher than 62! Found $value" }
+            require(value >= 0) { "Scale can't be smaller than 0! Found $value" }
+            this.root.addProperty("scale", value)
         }
 
         /**
@@ -275,8 +287,8 @@ sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
          * @param biomeId
          * @return
          */
-        fun addBiome(biomeId: String): CheckerboardBiomeSourceBuilder {
-            this.root.getAsJsonArray("biomes").add(biomeId)
+        fun addBiome(biomeId: Identifier): CheckerboardBiomeSourceBuilder {
+            this.root.getAsJsonArray("biomes").add(biomeId.toString())
             return this
         }
 
@@ -285,6 +297,7 @@ sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
         }
     }
 
+    @ArtificeDsl
     class FixedBiomeSourceBuilder : BiomeSourceBuilder<FixedBiomeSourceBuilder>(Identifier("fixed")) {
         override val me: FixedBiomeSourceBuilder
             get() = this
@@ -294,9 +307,10 @@ sealed class BiomeSourceBuilder<T: BiomeSourceBuilder<T>>(
          * @param biomeId
          * @return
          */
-        fun biome(biomeId: String): FixedBiomeSourceBuilder {
-            this.root.addProperty("biome", biomeId)
-            return this
-        }
+        fun id(biomeId: Identifier) = apply { id = biomeId }
+
+        var id: Identifier?
+        get() = root["biome"].asId
+        set(value) = root.addProperty("id", value.toString())
     }
 }
