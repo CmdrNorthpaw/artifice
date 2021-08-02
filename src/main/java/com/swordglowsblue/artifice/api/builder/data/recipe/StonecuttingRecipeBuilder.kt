aeba71b551a4e4106acfe0b3a50b8e0346245906
CmdrNorthpaw <thetuxkid@gmail.com>
2021-08-02 12:20:53 +0100

    Use DSL in StonecuttingRecipeBuilder

--- src/main/java/com/swordglowsblue/artifice/api/builder/data/recipe/StonecuttingRecipeBuilder.kt
+++ src/main/java/com/swordglowsblue/artifice/api/builder/data/recipe/StonecuttingRecipeBuilder.kt
@@ -1,32 +1,41 @@
 package com.swordglowsblue.artifice.api.builder.data.recipe
 
 import com.swordglowsblue.artifice.api.builder.JsonObjectBuilder
+import com.swordglowsblue.artifice.api.dsl.ArtificeDsl
+import com.swordglowsblue.artifice.api.dsl.Tag
+import com.swordglowsblue.artifice.api.util.IdUtils.asId
+import com.swordglowsblue.artifice.api.util.IdUtils.asItem
+import com.swordglowsblue.artifice.api.util.IdUtils.id
+import net.minecraft.item.Item
 import net.minecraft.util.Identifier
 
 /**
  * Builder for a stonecutting recipe (`namespace:recipes/id.json`).
  * @see [Minecraft Wiki](https://minecraft.gamepedia.com/Recipe.JSON_format)
  */
+@ArtificeDsl
 class StonecuttingRecipeBuilder : RecipeBuilder<StonecuttingRecipeBuilder>(Identifier("stonecutting")) {
     /**
      * Set the item being cut.
      * @param id The item ID.
      * @return this
      */
-    fun ingredientItem(id: Identifier): StonecuttingRecipeBuilder {
-        root.add("ingredient", JsonObjectBuilder().add("item", id.toString()).build())
-        return this
-    }
+    fun itemIngredient(item: Item) = apply { itemIngredient = item }
+
+    var itemIngredient: Item?
+    get() = extractItem("ingredient")
+    set(value) = root.addItem("ingredient", value)
 
     /**
      * Set the item being cut as any of the given tag.
      * @param id The tag ID.
      * @return this
      */
-    fun ingredientTag(id: Identifier): StonecuttingRecipeBuilder {
-        root.add("ingredient", JsonObjectBuilder().add("tag", id.toString()).build())
-        return this
-    }
+    fun tagIngredient(tag: Tag) = apply { tagIngredient = tag }
+
+    var tagIngredient: Tag?
+    get() = extractTag("ingredient")
+    set(value) = root.addTag("ingredient", value)
 
     /**
      * Set the item being cut as one of a list of options.
@@ -43,10 +52,11 @@ class StonecuttingRecipeBuilder : RecipeBuilder<StonecuttingRecipeBuilder>(Ident
      * @param id The item ID.
      * @return this
      */
-    fun result(id: Identifier): StonecuttingRecipeBuilder {
-        root.addProperty("result", id.toString())
-        return this
-    }
+    fun result(item: Item) = apply { result = item }
+
+    var result: Item?
+    get() = root["result"].asId?.asItem
+    set(value) = root.addProperty("result", value?.id.toString())
 
     /**
      * Set the number of items produced by this recipe.
@@ -57,5 +67,9 @@ class StonecuttingRecipeBuilder : RecipeBuilder<StonecuttingRecipeBuilder>(Ident
         root.addProperty("count", count)
         return this
     }
+
+    var count: Int?
+    get() = root["count"]?.asInt
+    set(value) = root.addProperty("count", value)
 
 }
