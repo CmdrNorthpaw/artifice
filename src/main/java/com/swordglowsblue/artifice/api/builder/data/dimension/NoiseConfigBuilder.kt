e8b9db563b7e83753c9a9287baf2cd96193f49b7
CmdrNorthpaw <thetuxkid@gmail.com>
2021-08-03 16:12:32 +0100

    Use DSL in NoiseConfigBuilder

--- src/main/java/com/swordglowsblue/artifice/api/builder/data/dimension/NoiseConfigBuilder.kt
+++ src/main/java/com/swordglowsblue/artifice/api/builder/data/dimension/NoiseConfigBuilder.kt
@@ -2,33 +2,64 @@ package com.swordglowsblue.artifice.api.builder.data.dimension
 
 import com.google.gson.JsonObject
 import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder
+import com.swordglowsblue.artifice.api.dsl.ArtificeDsl
 import com.swordglowsblue.artifice.api.util.Builder
 import java.util.function.Function
+import kotlin.jvm.Throws
 
+@ArtificeDsl
 class NoiseConfigBuilder : TypedJsonBuilder<JsonObject>(JsonObject(), { j: JsonObject -> j }) {
-    fun height(height: Int): NoiseConfigBuilder {
-        this.root.addProperty("height", height)
-        return this
-    }
+    fun height(height: Int) = apply { this.height = height }
+    
+    var height: Int?
+    get() = root["height"].asInt
+    set(value) = root.addProperty("height", value)
 
     fun minimumY(minimumY: Int): NoiseConfigBuilder {
         this.root.addProperty("min_y", minimumY)
         return this
     }
+    
+    var minimumY: Int?
+    get() = root["min_y"].asInt
+    set(value) = root.addProperty("min_y", value)
 
-    fun sizeHorizontal(sizeHorizontal: Int): NoiseConfigBuilder {
-        require(sizeHorizontal <= 4) { "sizeHorizontal can't be higher than 4! Found $sizeHorizontal" }
-        require(sizeHorizontal >= 1) { "sizeHorizontal can't be smaller than 1! Found $sizeHorizontal" }
-        require(sizeHorizontal != 3) { "sizeHorizontal should not be 3! See https://gist.github.com/misode/b83bfe4964e6bf53b2dd31b22ee94157 for information of why it should not be 3" }
-        this.root.addProperty("size_horizontal", sizeHorizontal)
-        return this
+    fun horizontalSize(size: Int) = apply { this.horizontalSize = size }
+
+    @set:OptIn(DangerousNoise::class)
+    var horizontalSize: Int?
+    get() = root["size_horizontal"].asInt
+    set(value) {
+        require(value != 3) { "value should not be 3! See https://gist.github.com/misode/b83bfe4964e6bf53b2dd31b22ee94157 for information of why it should not be 3" }
+        dangerousHorizontalSize = value
     }
 
-    fun sizeVertical(sizeVertical: Int): NoiseConfigBuilder {
-        require(sizeVertical <= 4) { "sizeVertical can't be higher than 4! Found $sizeVertical" }
-        require(sizeVertical >= 1) { "sizeVertical can't be smaller than 1! Found $sizeVertical" }
-        this.root.addProperty("size_vertical", sizeVertical)
-        return this
+    @RequiresOptIn
+    @Target(AnnotationTarget.PROPERTY_SETTER, AnnotationTarget.FUNCTION)
+    annotation class DangerousNoise
+
+    @DangerousNoise
+    fun dangerousHorizontalSize(noise: Int) = apply { this.dangerousHorizontalSize = noise }
+
+    @set:DangerousNoise
+    var dangerousHorizontalSize: Int?
+    get() = root["size_horizontal"].asInt
+    set(value) {
+        requireNotNull(value)
+        require(value <= 4) { "value can't be higher than 4! Found $value" }
+        require(value >= 1) { "value can't be smaller than 1! Found $value" }
+        root.addProperty("size_horizontal", value)
+    }
+
+    fun sizeVertical(size: Int) = apply { this.verticalSize = size }
+
+    var verticalSize: Int?
+    get() = root["size_vertical"].asInt
+    set(value) {
+        requireNotNull(value)
+        require(value <= 4) { "sizeVertical can't be higher than 4! Found $value" }
+        require(value >= 1) { "sizeVertical can't be smaller than 1! Found $value" }
+        this.root.addProperty("size_vertical", value)
     }
 
     fun densityFactor(densityFactor: Double): NoiseConfigBuilder {
@@ -36,59 +67,63 @@ class NoiseConfigBuilder : TypedJsonBuilder<JsonObject>(JsonObject(), { j: JsonO
         return this
     }
 
-    @Deprecated("use the double version instead")
+    var densityFactor: Double?
+    get() = root["density_factor"].asDouble
+    set(value) = root.addProperty("density_factor", value)
+
+    @Deprecated("use the double version instead",
+    replaceWith = ReplaceWith("densityOffset(densityOffset: Double)")
+    )
     fun densityOffset(densityOffset: Int): NoiseConfigBuilder {
         return this.densityOffset(densityOffset.toDouble())
     }
 
-    fun densityOffset(densityOffset: Double): NoiseConfigBuilder {
-        require(densityOffset <= 1) { "densityOffset can't be higher than 1! Found $densityOffset" }
-        require(densityOffset >= -1) { "densityOffset can't be smaller than -1! Found $densityOffset" }
-        this.root.addProperty("density_offset", densityOffset)
-        return this
-    }
+    fun densityOffset(densityOffset: Double) = apply { this.densityOffset = densityOffset }
 
-    fun simplexSurfaceNoise(simplexSurfaceNoise: Boolean): NoiseConfigBuilder {
-        this.root.addProperty("simplex_surface_noise", simplexSurfaceNoise)
-        return this
-    }
+    var densityOffset: Double?
+    get() = root["density_offset"].asDouble
+    set(value) = root.addProperty("density_offset", value)
 
-    fun randomDensityOffset(randomDensityOffset: Boolean): NoiseConfigBuilder {
-        this.root.addProperty("random_density_offset", randomDensityOffset)
-        return this
-    }
+    fun simplexSurfaceNoise(simplexSurfaceNoise: Boolean) = apply { this.simplexSurfaceNoise = simplexSurfaceNoise }
 
-    fun islandNoiseOverride(islandNoiseOverride: Boolean): NoiseConfigBuilder {
-        this.root.addProperty("island_noise_override", islandNoiseOverride)
-        return this
-    }
+    var simplexSurfaceNoise: Boolean?
+    get() = root["simplex_surface_noise"].asBoolean
+    set(value) = root.addProperty("simplex_surface_noide", value)
 
-    fun amplified(amplified: Boolean): NoiseConfigBuilder {
-        this.root.addProperty("amplified", amplified)
-        return this
-    }
+    fun randomDensityOffset(randomDensityOffset: Boolean) = apply { this.randomDensityOffset = randomDensityOffset }
+
+    var randomDensityOffset: Boolean?
+    get() = root["random_density_offset"].asBoolean
+    set(value) = root.addProperty("random_density_offset", value)
+
+    fun islandNoiseOverride(islandNoiseOverride: Boolean) = apply { this.islandNoiseOverride = islandNoiseOverride }
+
+    var islandNoiseOverride: Boolean?
+    get() = root["island_noise_override"].asBoolean
+    set(value) = root.addProperty("island_noise_override", value)
+
+    fun amplified(amplified: Boolean) = apply { this.isAmplified = amplified }
+
+    var isAmplified: Boolean?
+    get() = root["amplified"].asBoolean
+    set(value) = root.addProperty("amplified", value)
 
     /**
      * Build noise sampling config.
      */
-    fun sampling(noiseSamplingConfigBuilder: Builder<NoiseSamplingConfigBuilder>): NoiseConfigBuilder {
+    fun sampling(noiseSamplingConfigBuilder: Builder<NoiseSamplingConfigBuilder>) = apply {
         with("sampling", { JsonObject() }) { jsonObject: JsonObject ->
             NoiseSamplingConfigBuilder().apply(noiseSamplingConfigBuilder).buildTo(jsonObject)
         }
-        return this
     }
 
     /**
      * Build slide config.
      */
-    private fun slideConfig(
-        id: String,
-        processor: Builder<SlideConfigBuilder>
-    ): NoiseConfigBuilder {
+    private fun slideConfig(id: String, processor: Builder<SlideConfigBuilder>) = apply {
         with(id, { JsonObject() }) { jsonObject: JsonObject ->
             SlideConfigBuilder().apply(processor).buildTo(jsonObject)
         }
-        return this
     }
 
     /**
@@ -105,27 +140,31 @@ class NoiseConfigBuilder : TypedJsonBuilder<JsonObject>(JsonObject(), { j: JsonO
         return slideConfig("bottom_slide", slideConfigBuilder)
     }
 
+    @ArtificeDsl
     class NoiseSamplingConfigBuilder : TypedJsonBuilder<JsonObject>(JsonObject(), { j: JsonObject -> j }) {
-        fun xzScale(xzScale: Double): NoiseSamplingConfigBuilder {
-            require(xzScale <= 1000.0) { "xzScale can't be higher than 1000.0D! Found $xzScale" }
-            require(xzScale >= 0.001) { "xzScale can't be smaller than 0.001D! Found $xzScale" }
-            this.root.addProperty("xz_scale", xzScale)
-            return this
+        @Throws(IllegalArgumentException::class)
+        private fun requireScale(value: Double?) = value?.also { value ->
+            require(value <= 1000.0) { "value can't be higher than 1000.0D! Found $value" }
+            require(value >= 0.001) { "value can't be smaller than 0.001D! Found $value" }
         }
 
-        fun yScale(yScale: Double): NoiseSamplingConfigBuilder {
-            require(yScale <= 1000.0) { "yScale can't be higher than 1000.0D! Found $yScale" }
-            require(yScale >= 0.001) { "yScale can't be smaller than 0.001D! Found $yScale" }
-            this.root.addProperty("y_scale", yScale)
-            return this
-        }
+        fun xzScale(xzScale: Double) = apply { this.xzScale = xzScale }
 
-        fun xzFactor(xzFactor: Double): NoiseSamplingConfigBuilder {
-            require(xzFactor <= 1000.0) { "xzFactor can't be higher than 1000.0D! Found $xzFactor" }
-            require(xzFactor >= 0.001) { "xzFactor can't be smaller than 0.001D! Found $xzFactor" }
-            this.root.addProperty("xz_factor", xzFactor)
-            return this
-        }
+        var xzScale: Double?
+        get() = root["xz_scale"].asDouble
+        set(value) = root.addProperty("xz_scale", requireScale(value))
+
+        fun yScale(yScale: Double) = apply { this.yScale = yScale }
+
+        var yScale: Double?
+        get() = root["y_scale"].asDouble
+        set(value) = root.addProperty("y_scale", requireScale(value))
+
+        fun xzFactor(xzFactor: Double) = apply { this.xzFactor = xzFactor }
+
+        var xzFactor: Double?
+        get() = root["y_scale"].asDouble
+        set(value) = root.addProperty("y_scale", requireScale(value))
 
         fun yFactor(yFactor: Double): NoiseSamplingConfigBuilder {
             require(yFactor <= 1000.0) { "yFactor can't be higher than 1000.0D! Found $yFactor" }
@@ -133,24 +172,35 @@ class NoiseConfigBuilder : TypedJsonBuilder<JsonObject>(JsonObject(), { j: JsonO
             this.root.addProperty("y_factor", yFactor)
             return this
         }
+
+        var yFactor: Double?
+        get() = root["y_factor"].asDouble
+        set(value) = root.addProperty("y_factor", requireScale(value))
     }
 
+    @ArtificeDsl
     class SlideConfigBuilder : TypedJsonBuilder<JsonObject>(JsonObject(), { j: JsonObject -> j }) {
-        fun offset(offset: Int): SlideConfigBuilder {
-            this.root.addProperty("offset", offset)
-            return this
+        fun offset(offset: Int) = apply { this.offset = offset }
+
+        var offset: Int?
+        get() = root["offset"].asInt
+        set(value) = root.addProperty("offset", value)
+
+        fun size(size: Int) = apply { this.size = size }
+
+        var size: Int?
+        get() = root["size"].asInt
+        set(value) {
+            requireNotNull(value)
+            require(value <= 255) { "size can't be higher than 255! Found $value" }
+            require(value >= 0) { "size can't be smaller than 0! Found $value" }
+            this.root.addProperty("size", value)
         }
 
-        fun size(size: Int): SlideConfigBuilder {
-            require(size <= 255) { "size can't be higher than 255! Found $size" }
-            require(size >= 0) { "size can't be smaller than 0! Found $size" }
-            this.root.addProperty("size", size)
-            return this
-        }
+        fun target(target: Int) = apply { this.target = target }
 
-        fun target(target: Int): SlideConfigBuilder {
-            this.root.addProperty("target", target)
-            return this
-        }
+        var target: Int?
+        get() = root["target"].asInt
+        set(value) = root.addProperty("target", value)
     }
 }
