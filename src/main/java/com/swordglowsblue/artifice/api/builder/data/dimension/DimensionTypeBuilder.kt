f903ce5289cbff819a5253870ddeb87f886d34bb
CmdrNorthpaw <thetuxkid@gmail.com>
2021-08-03 11:18:18 +0100

    Use DSL in DimensionTypeBuilder

--- src/main/java/com/swordglowsblue/artifice/api/builder/data/dimension/DimensionTypeBuilder.kt
+++ src/main/java/com/swordglowsblue/artifice/api/builder/data/dimension/DimensionTypeBuilder.kt
@@ -2,13 +2,20 @@ package com.swordglowsblue.artifice.api.builder.data.dimension
 
 import com.google.gson.JsonObject
 import com.swordglowsblue.artifice.api.builder.TypedJsonBuilder
+import com.swordglowsblue.artifice.api.dsl.ArtificeDsl
+import com.swordglowsblue.artifice.api.dsl.Tag
 import com.swordglowsblue.artifice.api.resource.JsonResource
+import com.swordglowsblue.artifice.api.util.IdUtils.asId
+import com.swordglowsblue.artifice.api.util.addProperty
+import com.swordglowsblue.artifice.api.util.asTag
 import net.minecraft.util.Identifier
 import java.util.function.Function
 
+@ArtificeDsl
 class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
     JsonObject(),
-    { root: JsonObject -> JsonResource(root) }) {
+    { root: JsonObject -> JsonResource(root) }
+) {
     /**
      * Overworld -> false
      * Nether -> true
@@ -16,10 +23,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param ultrawarm
      * @return this
      */
-    fun ultrawarm(ultrawarm: Boolean): DimensionTypeBuilder {
-        root.addProperty("ultrawarm", ultrawarm)
-        return this
-    }
+    fun ultrawarm(ultrawarm: Boolean) = apply { this.ultrawarm = ultrawarm }
+
+    var ultrawarm: Boolean?
+    get() = root["ultrawarm"].asBoolean
+    set(value) = root.addProperty("ultrawarm", value)
 
     /**
      * Overworld -> true
@@ -28,10 +36,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param natural
      * @return this
      */
-    fun natural(natural: Boolean): DimensionTypeBuilder {
-        root.addProperty("natural", natural)
-        return this
-    }
+    fun isNatural(natural: Boolean) = apply { isNatural = natural }
+
+    var isNatural: Boolean?
+    get() = root["natural"].asBoolean
+    set(value) = root.addProperty("natural", value)
 
     /**
      * Overworld -> 1.0D
@@ -40,11 +49,15 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param coordinate_scale
      * @return this
      */
-    fun coordinate_scale(coordinate_scale: Double): DimensionTypeBuilder {
-        require(coordinate_scale >= 0.00001) { "Coordinate scale can't be higher than 0.00001D! Found $coordinate_scale" }
-        require(coordinate_scale <= 30000000) { "Coordinate scale can't be higher than 30000000D! Found $coordinate_scale" }
-        root.addProperty("coordinate_scale", coordinate_scale)
-        return this
+    fun coordinateScale(scale: Double) = apply { coordinateScale = scale }
+
+    var coordinateScale: Double?
+    get() = root["coordinate_scale"].asDouble
+    set(value) {
+        requireNotNull(value)
+        require(value >= 0.00001) { "Coordinate scale can't be higher than 0.00001D! Found $value" }
+        require(value <= 30000000) { "Coordinate scale can't be higher than 30000000D! Found $value" }
+        root.addProperty("coordinate_scale", value)
     }
 
     /**
@@ -54,11 +67,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param ambientLight
      * @return
      */
-    fun ambientLight(ambientLight: Float): DimensionTypeBuilder {
-//        if (ambientLight > 1.0F) throw new IllegalArgumentException("Ambient light can't be higher than 1.0F! Found " + ambientLight);
-        root.addProperty("ambient_light", ambientLight)
-        return this
-    }
+    fun ambientLight(ambientLight: Float) = apply { this.ambientLight = ambientLight }
+
+    var ambientLight: Float?
+    get() = root["ambient_light"].asFloat
+    set(value) = root.addProperty("ambient_light", value)
 
     /**
      * Overworld -> true
@@ -67,10 +80,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param hasSkylight
      * @return this
      */
-    fun hasSkylight(hasSkylight: Boolean): DimensionTypeBuilder {
-        root.addProperty("has_skylight", hasSkylight)
-        return this
-    }
+    fun hasSkylight(hasSkylight: Boolean) = apply { this.hasSkylight = hasSkylight }
+
+    var hasSkylight: Boolean?
+    get() = root["has_skylight"].asBoolean
+    set(value) = root.addProperty("has_skylight", value)
 
     /**
      * Overworld -> false
@@ -79,10 +93,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param hasCeiling
      * @return
      */
-    fun hasCeiling(hasCeiling: Boolean): DimensionTypeBuilder {
-        root.addProperty("has_ceiling", hasCeiling)
-        return this
-    }
+    fun hasCeiling(hasCeiling: Boolean) = apply { this.hasCeiling = hasCeiling }
+
+    var hasCeiling
+    get() = root["has_ceiling"].asBoolean
+    set(value) = root.addProperty("has_ceiling", value)
 
     /**
      * Overworld -> false
@@ -91,10 +106,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param hasEnderDragonFight
      * @return this
      */
-    fun hasEnderDragonFight(hasEnderDragonFight: Boolean): DimensionTypeBuilder {
-        root.addProperty("has_ender_dragon_fight", hasEnderDragonFight)
-        return this
-    }
+    fun hasEnderDragonFight(hasEnderDragonFight: Boolean) = apply { this.hasEnderDragonFight = hasEnderDragonFight }
+
+    var hasEnderDragonFight: Boolean?
+    get() = root["has_ender_dragon_fight"]?.asBoolean
+    set(value) = root.addProperty("has_ender_dragon_fight", value)
 
     /**
      * A block tag of which the blocks will not stop burning in the dimension.
@@ -104,30 +120,33 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param infiniburn The block tag id.
      * @return this
      */
-    fun infiniburn(infiniburn: Identifier): DimensionTypeBuilder {
-        root.addProperty("infiniburn", infiniburn.toString())
-        return this
-    }
+    fun infiniburn(infiniburn: Tag) = apply { this.infiniburn = infiniburn }
+
+    var infiniburn: Tag?
+    get() = root["infiniburn"].asTag
+    set(value) = root.addProperty("infiniburn", value)
 
     /**
      * Overworld -> 384
      * Nether -> 128
      * End -> 256
      */
-    fun minimumY(minimumY: Int): DimensionTypeBuilder {
-        root.addProperty("min_y", minimumY)
-        return this
-    }
+    fun minimumY(minimumY: Int) = apply { this.minimumY = minimumY }
+
+    var minimumY: Int
+    get() = root["min_y"].asInt
+    set(value) = root.addProperty("min_y", value)
 
     /**
      * Overworld -> 384
      * Nether -> 128
      * End -> 256
      */
-    fun height(height: Int): DimensionTypeBuilder {
-        root.addProperty("height", height)
-        return this
-    }
+    fun height(height: Int) = apply { this.height = height }
+
+    var height: Int?
+    get() = root["height"].asInt
+    set(value) = root.addProperty("height", value)
 
     /**
      * Overworld -> 256
@@ -136,10 +155,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param logicalHeight
      * @return
      */
-    fun logicalHeight(logicalHeight: Int): DimensionTypeBuilder {
-        root.addProperty("logical_height", logicalHeight)
-        return this
-    }
+    fun logicalHeight(logicalHeight: Int) = apply { this.logicalHeight = logicalHeight }
+
+    var logicalHeight: Int
+    get() = root["logical_height"].asInt
+    set(value) = root.addProperty("logical_height", value)
 
     /**
      * Set the fixed time of a dimension, do not set if you want a day-night cycle.
@@ -148,10 +168,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param fixedTime Time of the days in ticks
      * @return this
      */
-    fun fixedTime(fixedTime: Long): DimensionTypeBuilder {
-        root.addProperty("fixed_time", fixedTime)
-        return this
-    }
+    fun fixedTime(fixedTime: Long) = apply { this.fixedTime = fixedTime }
+
+    var fixedTime: Long
+    get() = root["fixed_time"].asLong
+    set(value) = root.addProperty("fixed_time", value)
 
     /**
      * Overworld -> true
@@ -160,10 +181,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param hasRaids
      * @return
      */
-    fun hasRaids(hasRaids: Boolean): DimensionTypeBuilder {
-        root.addProperty("has_raids", hasRaids)
-        return this
-    }
+    fun hasRaids(hasRaids: Boolean) = apply { this.hasRaids = hasRaids }
+
+    var hasRaids: Boolean?
+    get() = root["has_raids"].asBoolean
+    set(value) = root.addProperty("has_raids", value)
 
     /**
      * Overworld -> false
@@ -172,10 +194,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param respawnAnchorWork
      * @return
      */
-    fun respawnAnchorWorks(respawnAnchorWork: Boolean): DimensionTypeBuilder {
-        root.addProperty("respawn_anchor_works", respawnAnchorWork)
-        return this
-    }
+    fun respawnAnchorWorks(respawnAnchorWorks: Boolean) = apply { this.respawnAnchorWorks = respawnAnchorWorks }
+
+    var respawnAnchorWorks: Boolean?
+    get() = root["respawn_anchor_works"].asBoolean
+    set(value) = root.addProperty("respawn_anchor_works", value)
 
     /**
      * Overworld -> true
@@ -184,10 +207,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param bedWorks
      * @return this
      */
-    fun bedWorks(bedWorks: Boolean): DimensionTypeBuilder {
-        root.addProperty("bed_works", bedWorks)
-        return this
-    }
+    fun bedWorks(bedWorks: Boolean) = apply { this.bedWorks = bedWorks }
+
+    var bedWorks: Boolean?
+    get() = root["bed_works"].asBoolean
+    set(value) = root.addProperty("bed_works", value)
 
     /**
      * Overworld -> false
@@ -196,10 +220,11 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
      * @param piglinSafe
      * @return this
      */
-    fun piglinSafe(piglinSafe: Boolean): DimensionTypeBuilder {
-        root.addProperty("piglin_safe", piglinSafe)
-        return this
-    }
+    fun isPiglinSafe(piglinSafe: Boolean) = apply { this.isPiglinSafe = piglinSafe }
+
+    var isPiglinSafe: Boolean?
+    get() = root["piglin_safe"].asBoolean
+    set(value) = root.addProperty("piglin_safe", value)
 
     /**
      * Effects determine the sky effect of the dimension.
@@ -214,4 +239,8 @@ class DimensionTypeBuilder : TypedJsonBuilder<JsonResource<JsonObject>>(
         root.addProperty("effects", effects)
         return this
     }
+
+    var effects: Identifier?
+    get() = root["effects"].asId
+    set(value) = root.addProperty("effects", value.toString())
 }
